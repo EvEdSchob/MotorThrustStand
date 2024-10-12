b12411f648d1b7b66021a03a99e98e7facc95bc2
EvEdSchob <evedschob@live.com>
2024-10-11 22:33:31 -0400

    Create ThrustStand.ino

---
+++ TeensyCode/ThrustStand/ThrustStand.ino
@@ -0,0 +1,30 @@
+#include <Wire.h>
+
+//Sensor libraries
+
+
+void setup() {
+  // put your setup code here, to run once:
+  //Initialize sensors
+  //Initialize output
+  //Handshake with Pi
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  //Read serial for command
+
+}
+
+
+void lab() {
+  //Control throttle level directly as a percentage
+  //Check periodically for new commands
+}
+
+void dyno() {
+  //Control throttle as a fixed RPM and use PID to keep consistent
+}
