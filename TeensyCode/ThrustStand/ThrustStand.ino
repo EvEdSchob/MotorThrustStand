5b131e51fa31c91ff25a5b670777a1c7b648fcde
EvEdSchob <evedschob@live.com>
2024-10-25 16:26:34 -0400

    Add method for reading voltage

--- TeensyCode/ThrustStand/ThrustStand.ino
+++ TeensyCode/ThrustStand/ThrustStand.ino
@@ -31,7 +31,7 @@ bool msgRDY = false;
 //Serial send timer
 unsigned long lastSendTime = 0;
 
-//Thrust sensor variables
+//Tachometer variables
 volatile unsigned long lastPulseTime = 0;
 volatile unsigned long pulseCount = 0;
 unsigned long lastRPM = 0;
@@ -44,15 +44,22 @@ const unsigned long RPM_TIMEOUT = 2000000; //Timeout RPM to 0 after 2 seconds
 const unsigned long DEBOUNCE_MICROS = 1000; //1ms debouce (May need to be shorter)
 volatile unsigned long lastInterruptTime = 0;
 
+void FASTRUN rpmInterrupt() {
+  unsigned currentInterruptTime = micros();
+  //Debounce
+  if (currentInterruptTime - lastInterruptTime > DEBOUNCE_MICROS){
+    pulseCount++;
+    lastPulseTime = currentInterruptTime;
+    lastInterruptTime = currentInterruptTime;
+  }
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(2000000);
 
+  //Configure ADC
   analogReadResolution(12); //Use maximum resolution of the onboard ADC
-
-  //Configure load cell ADC
-  loadCell.begin(HX711_DAT, HX711_CLK); //Set DAT and CLK pins
-  loadCell.gain(128); //High-gain mode
 
   //Configure PWM output
   analogWriteFrequency(ESC_OUT, PWM_FREQ);
@@ -60,14 +67,25 @@ void setup() {
   pinMode(ESC_OUT, OUTPUT);
   analogWrite(ESC_OUT, MIN_PULSE); //Start the speed controller at minimum
 
+  //RPM "Trip Wire"
+  pinMode(IR_RECIV, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(IR_RECIV), rpmInterrupt, FALLING); 
 
-  
+  //Configure load cell ADC
+  loadCell.begin(HX711_DAT, HX711_CLK); //Set DAT and CLK pins
+  loadCell.gain(128); //High-gain mode
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   unsigned long currentTime = micros();
+
+  if(currentTime - lastRPMcalc >= RPM_CALC_INTERVAL){
+    calculateRPM(currentTime);
+    lastRPMcalc = currentTime;
+  }
+
   //Read serial for command
   receiveMSG();
 
@@ -75,6 +93,12 @@ void loop() {
     processMSG();
     msgRDY = false;
     inputBuffer = "";
+  }
+
+  //Send sensor data on send interval
+  if (currentTime - lastSendTime >= SEND_INTERVAL * 1000){
+    sendData();
+    lastSendTime = currentTime;
   }
 
 }
@@ -85,7 +109,6 @@ void calculateRPM(unsigned long currentTime){
   unsigned long localPulseCount = pulseCount;
   pulseCount = 0; //Reset pulse count for next reading;
   interrupts(); //Re-enable interrupts
-
   //Check if motor has stopped
   if(currentTime - localLastPulseTime > RPM_TIMEOUT){
     currentRPM = 0;
@@ -127,12 +150,21 @@ void sendData(){
   Serial.println(dataPackage);
 }
 
-
-void lab() {
-  //Control throttle level directly as a percentage
-  //Check periodically for new commands
+float readVoltage(int pin){
+  int rawValue = analogRead(pin);
+  return (rawValue / 4095.0) * 3.3; //Convert voltage based on 3.3v ref
 }
 
-void dyno() {
+String packageData(float pitot1, float pitot2, float current, float voltage, long rawLoadCell, float currentRPM){
+  return String(pitot1, 3) + "," + String(pitot2, 3) + "," + 
+           String(current, 3) + "," + String(v_in, 3) + "," + 
+           String(rawLoadCell) + "," + String(rpm, 1);  // RPM to 1 decimal place
+}
+
+void labRPM() {
+  //Control throttle level directly as a percentage
+}
+
+void dynoRPM() {
   //Control throttle as a fixed RPM and use PID to keep consistent
 }
